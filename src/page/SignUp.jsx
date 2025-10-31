6a267213222e3a31ae7f9c9a1729c1f28bd01bc1
timi15 <vargatimi505@gmail.com>
2025-10-31 20:56:12 +0100

    feat(auth): sign up with email and password

---
+++ src/page/SignUp.jsx
@@ -0,0 +1,147 @@
+import React, {useState} from 'react'
+import {
+    Box,
+    Button,
+    FormControl,
+    IconButton,
+    InputAdornment,
+    InputLabel,
+    OutlinedInput,
+    TextField,
+    Typography
+} from "@mui/material";
+import {createUserWithEmailAndPassword} from "firebase/auth";
+import {auth} from "../firebaseConfig"
+import GoogleIcon from '@mui/icons-material/Google';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
+
+export const SignUp = () => {
+
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        password: ''
+    });
+    const [showPassword, setShowPassword] = useState(false);
+
+    const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+    const handleMouseDownPassword = (event) => {
+        event.preventDefault();
+    };
+
+    const handleMouseUpPassword = (event) => {
+        event.preventDefault();
+    };
+
+    const signUpWithEmailAndPassword = async (e) => {
+        e.preventDefault();
+
+        await createUserWithEmailAndPassword(auth, formData.email, formData.password)
+            .then((userCredential) => {
+
+                const user = userCredential.user;
+                setFormData({name: '', email: '', password: ''})
+
+            })
+            .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                console.log(errorCode, " ", errorMessage);
+            });
+    }
+
+    return (
+        <div className='box'>
+
+            <Box
+                className="form"
+                component="form"
+                onSubmit={signUpWithEmailAndPassword}
+                autoComplete="off">
+
+                <Typography variant="h3">
+                    Sign Up
+                </Typography>
+
+                <TextField
+                    id="name"
+                    label="Name"
+                    name="name"
+                    value={formData.name}
+                    type="text"
+                    variant="outlined"
+                    required
+                    onChange={({target: {name, value}}) => setFormData({...formData, [name]: value})}
+                />
+
+                <TextField
+                    id="email"
+                    label="Email"
+                    name="email"
+                    value={formData.email}
+                    type="email"
+                    variant="outlined"
+                    required
+                    onChange={({target: {name, value}}) => setFormData({...formData, [name]: value})}
+                />
+
+                <FormControl variant="outlined" required>
+                    <InputLabel htmlFor="password">Password</InputLabel>
+                    <OutlinedInput
+                        id="password"
+                        name="password"
+                        label="Password"
+                        value={formData.password}
+                        type={showPassword ? 'text' : 'password'}
+                        onChange={({target: {name, value}}) => setFormData({...formData, [name]: value})}
+                        endAdornment={
+                            <InputAdornment position="end">
+                                <IconButton
+                                    className="password-show-icon"
+                                    aria-label={
+                                        showPassword ? 'hide the password' : 'display the password'
+                                    }
+                                    onClick={handleClickShowPassword}
+                                    onMouseDown={handleMouseDownPassword}
+                                    onMouseUp={handleMouseUpPassword}
+                                    edge="end"
+
+                                >
+                                    {showPassword ? <VisibilityOff/> : <Visibility/>}
+                                </IconButton>
+                            </InputAdornment>
+                        }
+                    />
+                </FormControl>
+
+                <Button
+                    size="large"
+                    variant="contained"
+                    type="submit">
+                    Sing Up
+                </Button>
+
+                <Typography
+                    variant="body1"
+                    component="h2">
+                    Already have an account? Sign In!
+                </Typography>
+
+                <div className="divider">
+                    <span>or</span>
+                </div>
+
+                <Button
+                    size="large"
+                    variant="contained">
+                    <GoogleIcon className="google-icon" fontSize="medium"/> Sing Up with Google
+                </Button>
+
+            </Box>
+
+        </div>
+
+    )
+}
