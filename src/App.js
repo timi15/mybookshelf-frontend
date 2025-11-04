2f996d6bd6ded90d85f3421a18ab589794ee202d
timi15 <vargatimi505@gmail.com>
2025-11-04 16:04:44 +0100

    feat(routing): add base app routing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import {Layout} from "./component/Layout";
+import {Home} from "./page/Home";
+import {Favourites} from "./page/Favourites";
+import {ToRead} from "./page/ToRead";
+import {Reviews} from "./page/Reviews";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <Layout>
+                <Routes>
+                    <Route path="/" element={<Home/>}> </Route>
+                    <Route path="/favourites" element={<Favourites/>}> </Route>
+                    <Route path="/to-read" element={<ToRead/>}> </Route>
+                    <Route path="/book-reviews" element={<Reviews/>}> </Route>
+                </Routes>
+            </Layout>
+        </Router>
+    );
 }
 
 export default App;
---
+++ src/Container.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import App from "./App";
+
+export const Container = ({children}) => {
+    return (
+        <App>
+            {children}
+        </App>
+    )
+}
